96387f5ce2aa6d40d734cb8e4cbe34ffd5b72c91
Simon Lenau <lenau@cispa.dem>
2024-05-22 10:02:51 +0200

    Updated CI script

--- run_dev-container.sh
+++ run_dev-container.sh
@@ -1,21 +1,43 @@
 #!/usr/bin/env bash
+source "submodules/gitlab-tokens/gitlab-tokens-init"
 
-source "submodules/gitlab_tokens/init"
+source "scripts/host/manage-gitlab-token"
 
-source scripts/host/gitlab_tokens
+printf "export %s\n" "WORKDIR=~/EXAMPLE/" "OUTDIR=~/EXAMPLE_OUTPUT/" >./.env
 
-printf "export %s\n" "WORKDIR=/EXAMPLE/" "OUTDIR=/EXAMPLE_OUTPUT/" > ./.env
+if [[ "${HOST}${HOSTNAME}" =~ "MacBook" ]]; then
+    {
+        docker_path="$(
+            printf -- "$(git_repo_info --type="registry")" |
+            perl -p0e "s/(5005)#/\${1}\//gmi" 
+        )"
 
-
-srun \
-    --job-name="${BASH_SOURCE[0]}${ZSH_ARGZERO} -- ${now}" \
-    --cpus-per-task=5 \
-    --time 300:00 \
-    --mail-type=ALL \
-    --mail-user=lenau@cispa.de \
-    --partition=r65257773x \
-    --container-image=$(git_repo_info --type="registry")dev-container:latest \
-    --no-container-mount-home \
-    --container-mounts="./.env:/.env" \
-    --container-entrypoint \
-    bash -c "/\$DEV_CONTAINER_DIR/dropbear_init"
+        echo "${CISPA_GITLAB_TOKEN}" |
+            docker login \
+                --username c01sile \
+                --password-stdin \
+                "${docker_path}" \
+                > \
+                /dev/null
+    }
+    docker pull ${docker_path}dev-container:latest
+    docker run \
+        --mount type=bind,source=./.env,destination=/.env,readonly \
+        -p 127.0.0.1:11782:11782/tcp \
+        -t -i \
+        ${docker_path}r-ver:latest \
+        bash -c /\$DEV_CONTAINER_DIR/run/dropbear_init
+else
+    srun \
+        --job-name="${BASH_SOURCE[0]}${ZSH_ARGZERO} -- ${now}" \
+        --cpus-per-task=5 \
+        --time 300:00 \
+        --mail-type=ALL \
+        --mail-user=lenau@cispa.de \
+        --partition=r65257773x \
+        --container-image=$(git_repo_info --type="registry")dev-container:latest \
+        --no-container-mount-home \
+        --container-mounts="./.env:/.env" \
+        --container-entrypoint \
+        bash -c /\$DEV_CONTAINER_DIR/run/dropbear_init
+fi
