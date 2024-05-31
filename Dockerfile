1a4504a62f211ce1cd6167cb3d9c35bfb93f2f43
Simon Lenau <lenau@cispa.dem>
2024-05-31 17:30:00 +0200

    Updated ssh_entrypoint and added symbolic link to /, updated logging and documentation

--- Dockerfile
+++ Dockerfile
@@ -66,5 +66,6 @@ COPY scripts/ssh_keys/* /$DEV_CONTAINER_DIR/run/.default_ssh_keys/
 # ────────────────────────────────── <end> ─────────────────────────────────── #
 
 RUN ln -s "${DEV_CONTAINER_DIR}/run/dropbear_init" /dropbear_init
+RUN ln -s "${DEV_CONTAINER_DIR}/run/ssh_entrypoint" /ssh_entrypoint
 
 CMD bash -c "/dropbear_init"
--- Examples/Start_Container.sh
+++ Examples/Start_Container.sh
@@ -6,12 +6,14 @@ printf "export %s\n" \
 
 source ./.env
 
-docker pull simonlenau/dev-container:containr_latest
+# docker pull simonlenau/dev-container:containr_latest
 
 # exit 0
 
 docker run \
     --mount type=bind,source=./.env,destination=/.env,readonly \
+    --mount type=bind,source=./scripts/run/ssh_key_check,destination=/dev-container/run/ssh_key_check \
+    --mount type=bind,source=./scripts/run/print_functions,destination=/dev-container/run/print_functions \
     -p 127.0.0.1:${dropbear_port}:${dropbear_port}/tcp \
     -t -i \
     simonlenau/dev-container:containr_latest \
